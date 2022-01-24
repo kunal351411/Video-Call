3c4788ecf16ae56798d179799cf7e168deca1fde
Kunal Kanotra <kanotrakunal030501@gmail.com>
2022-01-24 16:04:09 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const app = express();
+
+const server = require("http").createServer(app);
+
+const cors = require("cors");
+
+const io = require("socket.io")(server , {
+    cors : {
+        origin : "*",
+    methods : ["GET" , "POST"]
+    }
+});
+
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req,res) => {
+    res.send("Server is running")
+});
+
+io.on("connection" , (socket) => {
+
+    socket.emit("me", socket.id);
+    // console.log(socket.id);
+    // console.log("kk");
+
+    socket.on("disconnect" , () => {
+            socket.broadcast.emit("Call Ended");
+    });
+
+    socket.on("calluser" , ({ userToCall , signalData , from , name }) =>{
+        io.to(userToCall).emit("calluser" , { signal : signalData,from,name });
+    });
+
+    socket.on("answercall", (data) => {
+        io.to(data.to).emit("callaccepted" , data.signal);
+    });
+});
+
+server.listen(PORT , () => {
+    console.log(`Server listening on port ${PORT}`);
+});
